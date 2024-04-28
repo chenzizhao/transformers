4ff41542e250bda6edc78cad04d88bd062066bea
Zizhao Chen <ch272h@gmail.com>
2024-04-28 02:40:55 +0000

    add todos

---
+++ src/transformers/models/idefics2/convert_original_hf_checkpoint_to_flax.py
@@ -0,0 +1,6 @@
+# TODO(czz)
+# src/transformers/models/pix2struct/convert_pix2struct_original_pytorch_to_hf.py
+# src/transformers/models/fnet/convert_fnet_original_flax_checkpoint_to_pytorch.py
+# src/transformers/models/switch_transformers/convert_switch_transformers_original_flax_checkpoint_to_pytorch.py
+
+# modify text config to mistral flax verison
